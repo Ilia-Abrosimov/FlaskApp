1ae48c246dc83239fa6ae7c117d4c36da2a05df7
Ilia-Abrosimov <abrosimof93@gmail.com>
2021-04-28 21:13:50 +0300

    add put, patch, del for actors

--- src/resources/actors.py
+++ src/resources/actors.py
@@ -1,6 +1,11 @@
+from datetime import datetime
+
+from flask import request
 from flask_restful import Resource
+from marshmallow import ValidationError
 
 from src import db
+from src.database.models import Actor
 from src.schemas.actors import ActorSchema
 from src.services.actor_service import ActorService
 
@@ -18,13 +23,50 @@ class ActorListApi(Resource):
         return self.actor_schema.dump(actor), 200
 
     def post(self):
-        pass
+        try:
+            actor = self.actor_schema.load(request.json, session=db.session)
+        except ValueError as e:
+            return {'massage': str(e)}, 400
+        db.session.add(actor)
+        db.session.commit()
+        return self.actor_schema.dump(actor), 201
 
-    def put(self):
-        pass
+    def put(self, id):
+        actor = ActorService.fetch_actor_by_id(db.session, id)
+        if not actor:
+            return '', 404
+        try:
+            actor = self.actor_schema.load(request.json, instance=actor, session=db.session)
+        except ValidationError as e:
+            return {'massage': str(e)}, 400
+        db.session.add(actor)
+        db.session.commit()
+        return self.actor_schema.dump(actor), 200
 
-    def patch(self):
-        pass
 
-    def delete(self):
-        pass
+    def patch(self, id):
+        actor = db.session.query(Actor).filter_by(id=id).first()
+        if not actor:
+            return '', 404
+        actor_json = request.json
+        name = actor_json.get('name')
+        birthday = datetime.strptime(actor_json.get('birthday'), '%B %d, %Y') if actor_json.get(
+            'birthday') else None
+        is_active = actor_json.get('is_active')
+        if name:
+            actor.name = name
+        if birthday:
+            actor.birthday = birthday
+        if is_active:
+            actor.is_active = is_active
+        db.session.add(actor)
+        db.session.commit()
+        return {'massage': 'Updated successfully'}, 200
+
+    def delete(self, id):
+        actor = ActorService.fetch_actor_by_id(db.session, id)
+        if not actor:
+            return '', 404
+        db.session.delete(actor)
+        db.session.commit()
+        return '', 204
